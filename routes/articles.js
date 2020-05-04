6ad0ce2aae5c88896722a2f7fb0e8d5bf5a1d63d
Brian Sunday <brians931@gmail.com>
2020-05-04 12:20:22 +0300

    add initial endpoints for articles route

--- routes/articles.js
+++ routes/articles.js
@@ -1,4 +1,32 @@
 const express = require("express");
 const router = express.Router();
 
+// @route   GET api/articles
+// @desc    Get all club articles
+// @access  Public
+router.get("/", (req, res) => {
+  res.send("Get all articles");
+});
+
+// @route   POST api/articles
+// @desc    Create an article
+// @access  Private
+router.post("/", (req, res) => {
+  res.send("Create Article");
+});
+
+// @route   PUT api/articles/:id
+// @desc    Update an article
+// @access  Private
+router.put("/:id", (req, res) => {
+  res.send("Update article");
+});
+
+// @route   PUT api/articles/:id
+// @desc    Delete an article
+// @access  Private
+router.delete("/:id", (req, res) => {
+  res.send("Delete article");
+});
+
 module.exports = router;
