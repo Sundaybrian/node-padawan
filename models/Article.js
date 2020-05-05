1e7ffa5205ebdb92d76ee3c9562f143d434480aa
Brian Sunday <brians931@gmail.com>
2020-05-05 15:46:50 +0300

    add article model

---
+++ models/Article.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+const articleSchema = new mongoose.Schema({
+  author: {
+    type: mongoose.Schema.Types.ObjectId,
+    refs: "users",
+  },
+  club: {
+    type: mongoose.Schema.Types.ObjectId,
+    refs: "clubs",
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  subtitle: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  // extras:{
+  //     type:Array
+  // }
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Article", articleSchema);
