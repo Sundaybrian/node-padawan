70d27cc9d8978ae46807e6d6bd0ec0b924b3e9da
Brian Sunday <brians931@gmail.com>
2020-05-05 12:52:44 +0300

    add valiation for register route using express validator

--- routes/users.js
+++ routes/users.js
@@ -1,56 +1,71 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const { registerValidation } = require("../validation");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { check, validationResult } = require("express-validator");
 
 // @route   POST api/users
 // @desc    Register a user
 // @access  public
-router.post("/register", async (req, res) => {
-  // validate
-  const { error } = registerValidation(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+router.post(
+  "/register",
+  [
+    check("name", "Please enter a name with 6 or more characters").isLength({
+      min: 6,
+      max: 200,
+    }),
+    check("email", "Please enter valid a email").isEmail(),
+    check(
+      "password",
+      "Please enter a password with 6 or more characters"
+    ).isLength({ min: 8, max: 255 }),
+  ],
+  async (req, res) => {
+    // validate
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() });
 
-  // check if useremail exists on the db
-  const exists = await User.findOne({ email: req.body.email });
-  if (exists) return res.status(400).json({ msg: "Email already exists" });
+    // check if useremail exists on the db
+    const exists = await User.findOne({ email: req.body.email });
+    if (exists) return res.status(400).json({ msg: "Email already exists" });
 
-  // hashing password
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    // hashing password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
 
-  // create user instance
-  const user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: hashedPassword,
-  });
+    // create user instance
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
 
-  try {
-    // save to mongodb
-    const savedUser = await user.save();
-    const payload = {
-      user: {
-        id: savedUser._id,
-      },
-    };
+    try {
+      // save to mongodb
+      const savedUser = await user.save();
+      const payload = {
+        user: {
+          id: savedUser._id,
+        },
+      };
 
-    // generate token
-    jwt.sign(
-      payload,
-      process.env.TOKEN_SECRET,
-      { expiresIn: 36000 },
-      (err, token) => {
-        if (err) throw err;
-        res.json({ token });
-      }
-    );
-  } catch (error) {
-    console.error(error.message);
-    res.status(400).send(error);
+      // generate token
+      jwt.sign(
+        payload,
+        process.env.TOKEN_SECRET,
+        { expiresIn: 36000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (error) {
+      console.error(error.message);
+      res.status(400).send(error);
+    }
   }
-});
+);
 
 // @route   GET api/users
 // @desc    GEt all club users
