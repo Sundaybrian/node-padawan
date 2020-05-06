a14f0c7150fcba45bb08b336885e4dd5c0afae87
Brian Sunday <brians931@gmail.com>
2020-05-06 17:12:47 +0300

    setting up initial routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import "./App.css";
 
@@ -6,6 +7,12 @@ const App = () => {
   return (
     <>
       <Navbar title="Massive Fc" />
+      <ion-grid>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/" component={About} />
+        </Switch>
+      </ion-grid>
     </>
   );
 };
