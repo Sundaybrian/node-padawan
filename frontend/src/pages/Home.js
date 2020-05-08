2be1024133a7b7b44a6d980502714aa594cb095f
Brian Sunday <brians931@gmail.com>
2020-05-08 13:22:26 +0300

    split home page to columns

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,18 @@
 import React, { useContext } from "react";
 import Articles from "../components/articles/Articles";
+import { IonGrid, IonRow, IonCol } from "@ionic/react";
 
 const Home = () => {
-  return <Articles />;
+  return (
+    <IonGrid>
+      <IonRow>
+        <IonCol size="6"></IonCol>
+        <IonCol size="6">
+          <Articles />;
+        </IonCol>
+      </IonRow>
+    </IonGrid>
+  );
 };
 
 export default Home;
