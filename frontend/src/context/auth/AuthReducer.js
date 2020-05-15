ec337d86ba12fac77b1eb598a7e5939add356de7
Brian Sunday <brians931@gmail.com>
2020-05-15 12:14:14 +0300

    add the register success and fail to the authreducer

--- frontend/src/context/auth/AuthReducer.js
+++ frontend/src/context/auth/AuthReducer.js
@@ -11,6 +11,27 @@ import {
 
 export default (state, action) => {
   switch (action.type) {
+    case REGISTER_SUCCESS:
+      //set the token to local storage
+      localStorage.setItem("token", action.payload.token);
+      return {
+        ...state,
+        ...action.payload,
+        isAuthenticated: true,
+        loading: false,
+      };
+
+    case REGISTER_FAIL:
+      // remove token from local storage
+      localStorage.removeItem("token");
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false,
+        user: null,
+        error: action.payload,
+      };
     default:
       return state;
   }
