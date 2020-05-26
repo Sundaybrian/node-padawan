480981537d4317a7435c5b39af394c69bdc7598b
Brian Sunday <brians931@gmail.com>
2020-05-26 12:58:17 +0300

    created club articles schema

---
+++ models/ClubArticles.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const clubArticleSchema = new mongoose.Schema({
+  club: {
+    type: mongoose.Schema.Types.ObjectId,
+    refs: "clubs",
+  },
+  title: {
+    type: String,
+  },
+  url: {
+    type: String,
+  },
+  imageUrl: {
+    type: String,
+  },
+
+  createdAt: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("ClubArticleSchema", clubArticleSchema);
