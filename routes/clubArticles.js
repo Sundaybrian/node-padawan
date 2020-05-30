0867eeffd2a370e2259ceb5226088fd6dbd40ebe
Brian Sunday <brians931@gmail.com>
2020-05-30 22:28:49 +0300

    add pagination to club articles

--- routes/clubArticles.js
+++ routes/clubArticles.js
@@ -7,9 +7,39 @@ const ClubArticle = require("../models/ClubArticles");
 // @access public
 
 router.get("/", async (req, res) => {
+  // pagination options
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 5;
+
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+
+  const results = {};
+
   try {
-    const articles = await ClubArticle.find();
-    res.status(200).json(articles);
+    const articles = await ClubArticle.find()
+      .limit(limit)
+      .skip((page - 1) * limit)
+      .exec();
+
+    // fetch articles count
+    const count = await ClubArticle.countDocuments();
+
+    results.results = articles;
+
+    if (startIndex > 0) {
+      results.previousPage = page - 1;
+    }
+
+    if (endIndex < count) {
+      results.nexPage = page + 1;
+    }
+
+    results.totalPages = Math.ceil(count / limit);
+    results.currentPage = page;
+    results.limit = limit;
+
+    res.status(200).json(results);
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: error.message });
